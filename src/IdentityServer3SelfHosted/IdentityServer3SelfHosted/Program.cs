13642b55cf0febff591637b6d2e7b4576edbd794
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2017-05-09 15:09:14 +0430

    First init

---
+++ src/IdentityServer3SelfHosted/IdentityServer3SelfHosted/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace IdentityServer3SelfHosted
+{
+    class Program
+    {
+        static void Main()
+        {
+            string baseAddress = "http://localhost:9876/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpClient and make a request to api/values 
+                
+                Console.WriteLine($"Server start on {baseAddress}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
